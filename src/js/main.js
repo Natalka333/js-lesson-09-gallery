e838305a2072bbb4919fee7d9ff28d80f4a618ee
Natalia Trehubenko <nataly.111.0479@gmail.com>
2026-07-14 13:26:51 +0300

    fix lidrary

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,36 @@
 import "modern-normalize";
 import "../css/styles.css";
+
+import "simplelightbox/dist/simple-lightbox.css";
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from "./gallery-items";
+
+console.log(SimpleLightbox);
+
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+function makeGalleryItemsMarkup(items) {
+  return items
+    .map(({ original, preview, description }) => {
+      return `
+		<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+galleryEl.innerHTML = makeGalleryItemsMarkup(galleryItems);
+// Использую Node.js 24 и Vite 8. В этой версии simplelightbox импортируется как объект с default, поэтому библиотека корректно работает только через SimpleLightbox.default.
+const lightbox = new SimpleLightbox.default(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+// console.log(galleryItems);
